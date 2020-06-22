cd6f4171409fe9f98e3c4547437ac5c04f58f773
Julian Rüth <julian.rueth@fsfe.org>
2020-06-22 02:42:58 +0200

    vue-sfc-rollup generated boilerplate

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import RemoteComponent from '@/remote-component.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    RemoteComponent
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <remote-component />
+  </div>
+</template>
