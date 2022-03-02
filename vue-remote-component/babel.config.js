c808f00cc629ec5544b97ea2733de1b58ea4c8b1
Julian Rüth <julian.rueth@fsfe.org>
2022-03-01 23:35:23 -0600

    Upgrade vue-cli to latest version

--- vue-remote-component/babel.config.js
+++ vue-remote-component/babel.config.js
@@ -1,4 +1,4 @@
-const devPresets = ['@vue/babel-preset-app'];
+const devPresets = ['@vue/cli-plugin-babel/preset'];
 const buildPresets = ['@babel/preset-env'];
 module.exports = {
   presets: (process.env.NODE_ENV === 'development' ? devPresets : buildPresets),
